d7647a6b95cf1e8f5cd9e493d4a61d853f1589a9
Andrés Reyes <yoigaiden@gmail.com>
2020-12-18 08:13:45 -0600

    Creacion del proyecto

---
+++ Covid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid
+//
+//  Created by Mac2 on 18/12/20.
+//  Copyright © 2020 Mac2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
