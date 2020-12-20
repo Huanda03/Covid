4d17ce501a75c139bbd3f92aba7b37038ed8d510
Andrés Reyes <yoigaiden@gmail.com>
2020-12-20 11:00:07 -0600

    Se creo la interface grafica

--- Covid/ViewController.swift
+++ Covid/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var buscarTextField: UITextField!
+    @IBOutlet weak var muertesLabel: UILabel!
+    @IBOutlet weak var totalesLabel: UILabel!
+    @IBOutlet weak var recuperadosLabel: UILabel!
+    @IBOutlet weak var flagImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buscarTextField.delegate = self
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(buscarTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if buscarTextField.text != "" {
+            
+            return true
+        }else {
+            buscarTextField.placeholder = "Ingrese un pais"
+            return false
+        }
+    }
+    
 
-
+    @IBAction func buscarButton(_ sender: UIButton) {
+    }
 }
 
